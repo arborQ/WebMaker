039f7322a71dd731de095d04067ed097755698e3
arborQ <arbor@o2.pl>
2015-03-10 19:20:43 +0100

    add func

---
+++ Project/ConsoleApplication/ConsoleApplication/Calculator/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Calculator
+{
+    public class Converter
+    {
+        public float Convert(string input)
+        {
+            var output = 0f;
+            float.TryParse(input, out output);
+            return output;
+        }
+
+        public string Convert(float input)
+        {
+            return input.ToString("{0:N2}");
+        }
+    }
+}
---
+++ Project/ConsoleApplication/UnitTestProject/ConverterTest.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApplication.Calculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class ConverterTest
+    {
+
+        public void Test()
+        {
+            var converter = new Converter();
+        }
+    }
+}
